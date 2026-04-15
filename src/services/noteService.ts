1b5cb961a488674a238e66400104ca543d8cb112
Anna Smyrnova <anyto4ka777s@gmail.com>
2026-04-15 21:32:36 +0300

    fixed noteService file

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -7,13 +7,13 @@ axios.defaults.baseURL = "https://notehub-public.goit.study/api";
 axios.defaults.headers.common['Authorization'] = `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`;
 
 
-interface FetchNotesParams {
+interface FetchNotesResponce {
     notes: Note[];
     totalPages: number;
 }
 
-export const fetchNotes = async (query: string, page: number): Promise<FetchNotesParams> => {
-const response = await axios.get<FetchNotesParams>('/notes', {
+export const fetchNotes = async (query: string, page: number): Promise<FetchNotesResponce> => {
+const response = await axios.get<FetchNotesResponce>('/notes', {
     params: { search: query, 
         page: page},
 });
@@ -21,13 +21,13 @@ return response.data;
 };
 
 
-export const createNote = async (newNote: NewNote) => {
+export const createNote = async (newNote: NewNote): Promise<Note> => {
 const response = await axios.post<Note>('/notes', newNote);
 return response.data;
 };
 
 
-export const deleteNote = async (noteId: string) => {
+export const deleteNote = async (noteId: string): Promise<Note>  => {
 const response = await axios.delete<Note>(`/notes/${noteId}`);
 return response.data;
 }
